c39759dd8404b70a806a529e99e8d19491d2a7d6
smartsankar930 <37336699+smartsankar930@users.noreply.github.com>
2018-04-28 09:33:36 +0530

    Create alphabet.py

---
+++ code kata/alphabet.py
@@ -0,0 +1,10 @@
+def main():
+  
+  
+	ch = input("Enter any character: ")
+	if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+			print( "alphabet")
+	else:
+			print(" not an alphabet")
+if __name__ == '__main__':
+    main()
